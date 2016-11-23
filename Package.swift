dac1f2599b241a5f041e125dd2877d6c9249bd28
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-11-23 16:25:22 -0600

    Fixed repo name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CDB2Driver",
     dependencies: [
-      .Package(url: "https://github.com/rfdickerson/CODBC/blob/master/Package.swift", majorVersion: 0, minor: 0)
+      .Package(url: "https://github.com/rfdickerson/CODBC", majorVersion: 0, minor: 0)
     ]
 
 )
